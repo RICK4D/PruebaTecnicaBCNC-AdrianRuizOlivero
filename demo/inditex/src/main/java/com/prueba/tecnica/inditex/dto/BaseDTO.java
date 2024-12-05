d1ab829aaa45e2210fe6f05c9dcbe4190e42bf6a
R1CK4D <arox63@gmail.com>
2024-12-05 18:16:28 +0000

    feat(BaseDTO y BaseEntity): para poder aplicar una estructura bien modularizada es necesario plantear un enfoque Entity-DTO. El patrón DTO (Data Transformed Object) será de vital importancia en cualquier endpoint que se desee generar en el futuro. Además, se ha decidido crear estas clases como abstractas con el objetivo de poder reunir todos aquellos campos que sean comunes para cualquier Entity y DTO que se vaya a desarrollar

---
+++ demo/inditex/src/main/java/com/prueba/tecnica/inditex/dto/BaseDTO.java
@@ -0,0 +1,22 @@
+package com.prueba.tecnica.inditex.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Setter
+@Getter
+@SuperBuilder
+public abstract class BaseDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    private Long id;
+
+}
---
+++ demo/inditex/src/main/java/com/prueba/tecnica/inditex/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.prueba.tecnica.inditex.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Setter
+@Getter
+@SuperBuilder
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotNull
+    private Long id;
+
+}
