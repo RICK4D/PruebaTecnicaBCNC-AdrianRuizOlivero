1fe213f5c02a6a85a22efeaea5674f55fd74eb94
R1CK4D <arox63@gmail.com>
2024-12-06 12:49:14 +0000

    feat(AbstractService): creando clase que representa al conjunto de acciones comunes para un servicio,

---
+++ demo/inditex/src/main/java/com/prueba/tecnica/inditex/service/AbstractService.java
@@ -0,0 +1,45 @@
+package com.prueba.tecnica.inditex.service;
+
+import com.prueba.tecnica.inditex.copier.AbstractCopier;
+import com.prueba.tecnica.inditex.dto.BaseDTO;
+import com.prueba.tecnica.inditex.entity.BaseEntity;
+import com.prueba.tecnica.inditex.repository.IRepository;
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.Optional;
+
+public abstract class AbstractService<E extends BaseEntity, D extends BaseDTO> {
+
+    protected abstract IRepository<E, Long> getRepository();
+    protected abstract AbstractCopier<E, D> getCopier();
+
+    // Guardar una entidad (desde DTO)
+    protected D save(@NonNull D dto) {
+        E entity = getCopier().toEntity(dto);
+        E savedEntity = getRepository().save(entity);
+        return getCopier().toDTO(savedEntity);
+    }
+
+    // Buscar por ID (devuelve un Optional del DTO)
+    protected Optional<D> findById(@NonNull Long id) {
+        return getRepository().findById(id).map(getCopier()::toDTO);
+    }
+
+    // Listar todas las entidades (como lista de DTOs)
+    protected List<D> findAll() {
+        return getRepository().findAll().stream().map(getCopier()::toDTO).toList();
+    }
+
+    // Eliminar por ID
+    protected void deleteById(@NonNull Long id) {
+        getRepository().deleteById(id);
+    }
+
+    // Actualizar una entidad (desde DTO)
+    protected D update(@NonNull D dto) {
+        E entity = getCopier().toEntity(dto);
+        E updatedEntity = getRepository().update(entity);
+        return getCopier().toDTO(updatedEntity);
+    }
+}
