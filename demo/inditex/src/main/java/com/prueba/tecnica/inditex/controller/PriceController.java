73127337161c38f76fb6c0509b95856c18053b57
R1CK4D <arox63@gmail.com>
2024-12-11 14:39:20 +0000

    chore: eliminando endpoints no solicitados. Dejamos el propuesto en el enunciado junto con las búsquedas de todos los precios y los ids

--- demo/inditex/src/main/java/com/prueba/tecnica/inditex/controller/PriceController.java
+++ demo/inditex/src/main/java/com/prueba/tecnica/inditex/controller/PriceController.java
@@ -3,6 +3,7 @@ package com.prueba.tecnica.inditex.controller;
 import com.prueba.tecnica.inditex.dto.PriceDTO;
 import com.prueba.tecnica.inditex.exception.EntityNotFoundException;
 import com.prueba.tecnica.inditex.service.PriceService;
+import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -53,78 +54,7 @@ public class PriceController {
         return ResponseEntity.ok(priceService.findAll());
     }
 
-    /**
-     * Create price response entity.
-     *
-     * @param priceDTO the price dto
-     * @return the response entity
-     * @throws EntityNotFoundException  the entity not found exception
-     * @throws IllegalArgumentException the illegal argument exception
-     */
-    @PostMapping
-    public ResponseEntity<PriceDTO> createPrice(@RequestBody PriceDTO priceDTO) throws EntityNotFoundException, IllegalArgumentException {
-        return ResponseEntity.ok(priceService.save(priceDTO));
-    }
 
-    /**
-     * Update price response entity.
-     *
-     * @param id       the id
-     * @param priceDTO the price dto
-     * @return the response entity
-     * @throws EntityNotFoundException  the entity not found exception
-     * @throws IllegalArgumentException the illegal argument exception
-     */
-    @PutMapping("/{id}")
-    public ResponseEntity<PriceDTO> updatePrice(@PathVariable Long id, @RequestBody PriceDTO priceDTO)
-            throws EntityNotFoundException, IllegalArgumentException {
-        priceDTO.setId(id);
-        return ResponseEntity.ok(priceService.update(priceDTO));
-    }
-
-    /**
-     * Delete price response entity.
-     *
-     * @param id the id
-     * @return the response entity
-     * @throws EntityNotFoundException the entity not found exception
-     */
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePrice(@PathVariable Long id) throws EntityNotFoundException {
-        priceService.deleteById(id);
-        return ResponseEntity.noContent().build();
-    }
-
-    /**
-     * Find by product id and brand id response entity.
-     *
-     * @param productId the product id
-     * @param brandId   the brand id
-     * @return the response entity
-     */
-    @GetMapping("/search")
-    public ResponseEntity<List<PriceDTO>> findByProductIdAndBrandId(@RequestParam Long productId,
-                                                                    @RequestParam Long brandId) {
-        return ResponseEntity.ok(priceService.findByProductIdAndBrandId(productId, brandId));
-    }
-
-    /**
-     * Find active price response entity.
-     *
-     * @param productId the product id
-     * @param brandId   the brand id
-     * @param date      the date
-     * @return the response entity
-     */
-// Encontrar el precio activo en una fecha específica
-    @GetMapping("/active")
-    public ResponseEntity<PriceDTO> findActivePrice(@RequestParam Long productId, @RequestParam Long brandId,
-            @RequestParam String date) {
-        LocalDateTime dateTime = LocalDateTime.parse(date);
-        return priceService.findActivePriceByProductIdAndBrandIdAndDate(productId, brandId, dateTime)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
 
     /**
      * Gets applicable price.
