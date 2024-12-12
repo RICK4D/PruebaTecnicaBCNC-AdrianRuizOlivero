ce9877d161620df9b336abad5b103be7227f4e64
R1CK4D <arox63@gmail.com>
2024-12-12 16:40:09 +0000

    feat(@Generated): creando anotación que permite ignorar a JaCoCo en el lugar donde se aplique

---
+++ src/main/java/com/prueba/tecnica/inditex/annotation/Generated.java
@@ -0,0 +1,12 @@
+package com.prueba.tecnica.inditex.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({TYPE, METHOD, CONSTRUCTOR})
+public @interface Generated {
+}
